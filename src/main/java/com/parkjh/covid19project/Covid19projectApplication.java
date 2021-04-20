3ac7d433a6fae5c6f301fbdaa92143a4491b61c4
parkjh <tkarnrrns200@naver.com>
2021-04-20 21:34:51 +0900

    Initial commit

---
+++ src/main/java/com/parkjh/covid19project/Covid19projectApplication.java
@@ -0,0 +1,13 @@
+package com.parkjh.covid19project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Covid19projectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkjh/covid19project/Covid19projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkjh.covid19project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
